7b898bd4941b87d867e15b30901bb8282f28768c
Jessica Gladstone <jcg321@comcast.net>
2019-04-07 15:38:23 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
    def create
        if !params[:name] || params[:name] == ""
-        redirect_to '/login'
+        redirect_to '/sessions/new'
      else
        session[:name] = params[:name]
        redirect_to '/secrets'
