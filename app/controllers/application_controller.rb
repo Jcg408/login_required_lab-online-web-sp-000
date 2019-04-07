6267eeeb826c3d3a49139d4388f60a058bd03074
Jessica Gladstone <jcg321@comcast.net>
2019-04-07 15:59:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
     def current_user
-     if !session[:name]
-       session[:name] = params[:name]
-     else
        session[:name]
      end
-   end
+   
  end
