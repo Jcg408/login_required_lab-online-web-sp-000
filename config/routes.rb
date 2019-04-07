4a96190ee406d4a8ad1837b7640758e7e82d1964
Jessica Gladstone <jcg321@comcast.net>
2019-04-07 15:30:48 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
 
   get 'secrets/show'
-  
+
   get 'sessions/destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
