d026e8ae20169a16245f10a32bec5475287ad762
Jessica Gladstone <jcg321@comcast.net>
2019-04-07 14:56:19 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
         redirect_to '/sessions/new'
      else
        session[:name] = params[:name]
-       redirect_to '/'
+       redirect_to '/login'
      end
    end
 
