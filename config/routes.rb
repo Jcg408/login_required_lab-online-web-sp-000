9f148e3a278e3ca0e947c782baa5a46da28a4a26
Jessica Gladstone <jcg321@comcast.net>
2019-04-07 15:01:12 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
 
   post '/login' => 'sessions#create'
-
-  get 'sessions/new'
-
+  
   get 'sessions/create'
 
   get 'sessions/destroy'
