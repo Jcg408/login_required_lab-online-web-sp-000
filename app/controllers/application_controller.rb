54e89c0ce002915e6f2f6ce96e6ed08eea318228
Jessica Gladstone <jcg321@comcast.net>
2019-04-07 14:43:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def current_user
-   if !session[:name]
-     session[:name] = params[:name]
-   else
-     session[:name]
+    def current_user
+     if !session[:name]
+       session[:name] = params[:name]
+     else
+       session[:name]
+     end
    end
  end
